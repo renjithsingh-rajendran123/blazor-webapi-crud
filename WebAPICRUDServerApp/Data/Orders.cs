6907341e86b349a6c0c01e197d66cc6020be6540
renjithsingh-rajendran123 <40191389+renjithsingh-rajendran123@users.noreply.github.com>
2021-05-27 20:12:09 +0530

    Added Project

---
+++ WebAPICRUDServerApp/Data/Orders.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAPICRUDServerApp.Data
+{
+    public partial class Orders
+    {
+        public long OrderId { get; set; }
+        public string CustomerId { get; set; }
+        public int? Freight { get; set; }
+        public DateTime? OrderDate { get; set; }
+    }
+}
---
+++ WebAPICRUDServerApp/OrdersController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPICRUDServerApp.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPICRUDServerApp
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private OrdersDetailsContext _context;
+        public OrdersController(OrdersDetailsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<OrdersController>
+        [HttpGet]
+        public object Get()
+        {
+            return new { Items = _context.Orders, Count = _context.Orders.Count() };
+        }
+
+        // POST api/<OrdersController>
+        [HttpPost]
+        public void Post([FromBody] Orders book)
+        {
+            _context.Orders.Add(book);
+            _context.SaveChanges();
+        }
+
+        // PUT api/<OrdersController>
+        [HttpPut]
+        public void Put(long id, [FromBody] Orders book)
+        {
+            Orders _book = _context.Orders.Where(x => x.OrderId.Equals(book.OrderId)).FirstOrDefault();
+            _book.CustomerId = book.CustomerId;
+            _book.Freight = book.Freight;
+            _book.OrderDate = book.OrderDate;
+            _context.SaveChanges();
+        }
+
+        // DELETE api/<OrdersController>
+        [HttpDelete("{id}")]
+        public void Delete(long id)
+        {
+            Orders _book = _context.Orders.Where(x => x.OrderId.Equals(id)).FirstOrDefault();
+            _context.Orders.Remove(_book);
+            _context.SaveChanges();
+        }
+    }
+}
